44697f14f803e1bacc1356f0e7009ca9e81fdabd
Udit Manektala <uditmanektala@gmail.com>
2016-05-11 08:02:33 -0700

    Adding avatar

--- app/index.js
+++ app/index.js
@@ -1,14 +1,39 @@
+var USER_DATA = {
+  name: 'Udit',
+  username: 'udit99',
+  image: 'https://avatars2.githubusercontent.com/u/77560?v=3&s=460'
+}
 var React = require("react");
 var ReactDOM = require("react-dom");
-var HelloWorld = React.createClass({
+
+var Avatar = React.createClass({
   render: function(){
     return (
-      <div>Hello Worldz</div>
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
+  }
+})
+
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href="http://www.github.com/{this.props.username}">{this.props.username}</a>
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld/>,
+  <Avatar user={USER_DATA}/>,
   document.getElementById("app")
-);
+)
